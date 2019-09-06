7e867bb621dbaf1a95324dfc8911d0357b342e46
pulina-paulina <ekaterina.paulina@yandex.ru>
2019-09-06 18:34:05 +0300

    new

---
+++ AspCore/wwwroot/lib/jq/install_Masonry.js
@@ -0,0 +1,6 @@
+﻿jQuery(documrnt).ready(funtion($) { //-используем js файл
+    $(".container_cards").masonry({ //родительский блок 
+        itemSelector: ".card", //дочерний блок
+        columnWidth: 50
+    });
+});
